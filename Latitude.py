6825598b579df532c7ed74b067afe3791770fe40
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-16 14:59:55 -0900

    reformat with black

--- Latitude.py
+++ Latitude.py
@@ -5,43 +5,46 @@ import arcpy
 Calculates a latitude raster; each cell gets the latitude of its geographic location
 """
 
-class Latitude():
 
+class Latitude:
     def __init__(self):
         self.name = "Latitude Function"
-        self.description = ("Provides a raster of the latitude of "
-                            "each cell in the input")
+        self.description = (
+            "Provides a raster of the latitude of " "each cell in the input"
+        )
         self.sr = None
         self.cgs = None
         self.cellsize = None
 
-
     def getParameterInfo(self):
         return [
             {
-                'name': 'input',
-                'dataType': 'raster',
-                'value': None,
-                'required': True,
-                'displayName': "input raster",
-                'description': ("Any georeferenced raster.")
+                "name": "input",
+                "dataType": "raster",
+                "value": None,
+                "required": True,
+                "displayName": "input raster",
+                "description": ("Any georeferenced raster."),
             },
         ]
 
-
     def getConfiguration(self, **scalars):
         return {
-          'inheritProperties': 2 | 4 | 8,            # inherit nodata, size, resample type, not pixel type. 
-          'invalidateProperties':  1 | 2 | 4 | 8,    # reset everything on the parent dataset.
-          'resampling': True,                        # process at request resolution
+            "inheritProperties": 2
+            | 4
+            | 8,  # inherit nodata, size, resample type, not pixel type.
+            "invalidateProperties": 1
+            | 2
+            | 4
+            | 8,  # reset everything on the parent dataset.
+            "resampling": True,  # process at request resolution
         }
-
 
     def updateRasterInfo(self, **kwargs):
         # extent and spatial reference are for map coordinates, not native image coords.
-        extent = kwargs['input_info']['extent']
-        self.cellsize = kwargs['input_info']['cellSize'] # Tuple(2x Floats)
-        epsg = kwargs['input_info']['spatialReference'] # int EPSG code
+        extent = kwargs["input_info"]["extent"]
+        self.cellsize = kwargs["input_info"]["cellSize"]  # Tuple(2x Floats)
+        epsg = kwargs["input_info"]["spatialReference"]  # int EPSG code
         self.sr = arcpy.SpatialReference()
         self.sr.loadFromString(str(epsg))
         self.cgs = self.sr.GCS
@@ -54,43 +57,49 @@ class Latitude():
             p = arcpy.PointGeometry(arcpy.Point(xmax, ymax), self.sr, False, False)
             q = p.projectAs(self.cgs)
             ymax = q.firstPoint.Y
-        kwargs['output_info']['bandCount'] =  1     # output is a single band raster
-        kwargs['output_info']['pixelType'] = 'f4'   # output is a 32bit floating point number
-        kwargs['output_info']['statistics'] = ({'minimum': ymin, 'maximum':ymax}, )    # we could get something from the input extents
-        kwargs['output_info']['histogram'] = ()
+        kwargs["output_info"]["bandCount"] = 1  # output is a single band raster
+        kwargs["output_info"][
+            "pixelType"
+        ] = "f4"  # output is a 32bit floating point number
+        kwargs["output_info"]["statistics"] = (
+            {"minimum": ymin, "maximum": ymax},
+        )  # we could get something from the input extents
+        kwargs["output_info"]["histogram"] = ()
         return kwargs
 
-
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        nRows, nCols = shape if len(shape) == 2 else shape[1:]      # dimensions of request pixel block
+        nRows, nCols = (
+            shape if len(shape) == 2 else shape[1:]
+        )  # dimensions of request pixel block
         # tlc is the row, column number, not the projected or geographic coordinates
-        e = props['extent']      # XMin, YMin, XMax, YMax values in the output coordinates
-        h = props['height']      # Number of rows in the parent raster
-        w = props['width']       # Number of cols in the parent raster
-        dY = (e[3]-e[1])/h       # cell height
-        dX = (e[2]-e[0])/w       # cell width
-        #dX,dY = self.cellsize
+        e = props["extent"]  # XMin, YMin, XMax, YMax values in the output coordinates
+        h = props["height"]  # Number of rows in the parent raster
+        w = props["width"]  # Number of cols in the parent raster
+        dY = (e[3] - e[1]) / h  # cell height
+        dX = (e[2] - e[0]) / w  # cell width
+        # dX,dY = self.cellsize
         left_col, top_row = tlc
-        yMax = e[3]-top_row*dY    # top-left corner of request on map
+        yMax = e[3] - top_row * dY  # top-left corner of request on map
         if self.cgs != self.sr:
-            xMin = e[0]+left_col*dX
+            xMin = e[0] + left_col * dX
             p = arcpy.PointGeometry(arcpy.Point(xMin, yMax), self.sr, False, False)
             q = p.projectAs(self.cgs)
             yMax = q.firstPoint.Y
         # Create a raster with 0 in the top row, 1 in the next row, etc.
-        x = np.linspace(0, nCols-1, nCols)
-        y = np.linspace(0, nRows-1, nRows)
-        row_matrix = np.meshgrid(x,y)[1]
+        x = np.linspace(0, nCols - 1, nCols)
+        y = np.linspace(0, nRows - 1, nRows)
+        row_matrix = np.meshgrid(x, y)[1]
 
         latitude = yMax - row_matrix * dY
-        pixelBlocks['output_pixels'] = latitude.astype(props['pixelType'], copy=False)
+        pixelBlocks["output_pixels"] = latitude.astype(props["pixelType"], copy=False)
         return pixelBlocks
-
 
     def updateKeyMetadata(self, names, bandIndex, **keyMetadata):
         if bandIndex == -1:
-            keyMetadata['datatype'] = 'Processed'               # outgoing raster is now 'Processed'
+            keyMetadata["datatype"] = "Processed"  # outgoing raster is now 'Processed'
         elif bandIndex == 0:
-            keyMetadata['wavelengthmin'] = None                 # reset inapplicable band-specific key metadata
-            keyMetadata['wavelengthmax'] = None
+            keyMetadata[
+                "wavelengthmin"
+            ] = None  # reset inapplicable band-specific key metadata
+            keyMetadata["wavelengthmax"] = None
         return keyMetadata
--- VectorRuggednessMeasure.py
+++ VectorRuggednessMeasure.py
@@ -28,75 +28,91 @@ based on a geomorphological method for measuring vector dispersion that is less
 correlated with slope...
 """
 
-class VectorRuggednessMeasure():
 
+class VectorRuggednessMeasure:
     def __init__(self):
         self.name = "Vector Ruggedness Measure"
-        self.description = ("Calculates the ruggedness of the terrain by measuring "
-                            "the amount of dispersal of the normal vectors "
-                             "of the terrain in the neighborhood of a location")
+        self.description = (
+            "Calculates the ruggedness of the terrain by measuring "
+            "the amount of dispersal of the normal vectors "
+            "of the terrain in the neighborhood of a location"
+        )
         self.neighborhood_size = 3
 
     def getParameterInfo(self):
         return [
             {
-                'name': 'slope',
-                'dataType': 'raster',
-                'value': None,
-                'required': True,
-                'displayName': "Slope raster",
-                'description': ("A raster of slope values for the terrain, in degrees "
-                                "with 0 = flat and 90 = vertical")
+                "name": "slope",
+                "dataType": "raster",
+                "value": None,
+                "required": True,
+                "displayName": "Slope raster",
+                "description": (
+                    "A raster of slope values for the terrain, in degrees "
+                    "with 0 = flat and 90 = vertical"
+                ),
             },
             {
-                'name': 'aspect',
-                'dataType': 'raster',
-                'value': None,
-                'required': True,
-                'displayName': "Aspect raster",
-                'description': ("A raster of aspect value for the terrain, in degrees "
-                                "with north = 0, increasing clockwise")
+                "name": "aspect",
+                "dataType": "raster",
+                "value": None,
+                "required": True,
+                "displayName": "Aspect raster",
+                "description": (
+                    "A raster of aspect value for the terrain, in degrees "
+                    "with north = 0, increasing clockwise"
+                ),
             },
             {
-                'name': 'size',
-                'dataType': 'numeric',
-                'value': 3,
-                'required': False,
-                'displayName': "Neighborhood Size",
-                'description': ("A square of size x size  is considered to determine "
-                                "the ruggedness at the center of the square.")
+                "name": "size",
+                "dataType": "numeric",
+                "value": 3,
+                "required": False,
+                "displayName": "Neighborhood Size",
+                "description": (
+                    "A square of size x size  is considered to determine "
+                    "the ruggedness at the center of the square."
+                ),
             },
         ]
 
-
     def getConfiguration(self, **scalars):
         return {
-          'inheritProperties': 2 | 4 | 8,          # inherit nodata, size, resample type, not pixel type. 
-          'invalidateProperties':  1 | 2 | 4 | 8,  # reset everything on the parent dataset.
-          'resampling': True,                      # process at request resolution
+            "inheritProperties": 2
+            | 4
+            | 8,  # inherit nodata, size, resample type, not pixel type.
+            "invalidateProperties": 1
+            | 2
+            | 4
+            | 8,  # reset everything on the parent dataset.
+            "resampling": True,  # process at request resolution
         }
 
-
     def updateRasterInfo(self, **kwargs):
-        self.neighborhood_size = kwargs.get('size', 3)
+        self.neighborhood_size = kwargs.get("size", 3)
 
-        kwargs['output_info']['bandCount'] =  1     # output is a single band raster
-        kwargs['output_info']['pixelType'] = 'f4'   # output is a 32bit floating point number
-        kwargs['output_info']['statistics'] = ({'minimum': 0.0, 'maximum': 1.0}, )  # we know something about the range of the outgoing raster.
-        kwargs['output_info']['histogram'] = ()
+        kwargs["output_info"]["bandCount"] = 1  # output is a single band raster
+        kwargs["output_info"][
+            "pixelType"
+        ] = "f4"  # output is a 32bit floating point number
+        kwargs["output_info"]["statistics"] = (
+            {"minimum": 0.0, "maximum": 1.0},
+        )  # we know something about the range of the outgoing raster.
+        kwargs["output_info"]["histogram"] = ()
         return kwargs
 
-
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        slope = np.array(pixelBlocks['slope_pixels'], dtype='f4', copy=False)[0] # limit to first (only) band
-        aspect = np.array(pixelBlocks['aspect_pixels'], dtype='f4', copy=False)[0]
-        dr = np.pi/180.0  # degree to radian
+        slope = np.array(pixelBlocks["slope_pixels"], dtype="f4", copy=False)[
+            0
+        ]  # limit to first (only) band
+        aspect = np.array(pixelBlocks["aspect_pixels"], dtype="f4", copy=False)[0]
+        dr = np.pi / 180.0  # degree to radian
         slope_rad = slope * dr
         aspect_rad = aspect * dr
         xy = np.sin(slope_rad)
         z = np.cos(slope_rad)
-        x = np.where(aspect == -1, 0.0, np.sin(aspect_rad)*xy)
-        y = np.where(aspect == -1, 0.0, np.cos(aspect_rad)*xy)
+        x = np.where(aspect == -1, 0.0, np.sin(aspect_rad) * xy)
+        y = np.where(aspect == -1, 0.0, np.cos(aspect_rad) * xy)
         kernel = np.ones((self.neighborhood_size, self.neighborhood_size))
         kernel_size = self.neighborhood_size * self.neighborhood_size
         # focal statistics sum (used in the original tool) is the same as an image
@@ -106,19 +122,20 @@ class VectorRuggednessMeasure():
         #   cells outside the boundary will always have a zero value -- no contribution
         # FIXME: need to handle nodata
         #   original tool returned nodata if any cell in the neighborhood was nodata
-        x_sum = ndimage.convolve(x, kernel) #, mode='constant', cval=0.0)
-        y_sum = ndimage.convolve(y, kernel) #, mode='constant', cval=0.0)
-        z_sum = ndimage.convolve(z, kernel) #, mode='constant', cval=0.0)
-        total = np.sqrt(x_sum*x_sum + y_sum*y_sum + z_sum*z_sum)
-        ruggedness = 1.0 - (total/kernel_size)
-        pixelBlocks['output_pixels'] = ruggedness.astype(props['pixelType'], copy=False)
+        x_sum = ndimage.convolve(x, kernel)  # , mode='constant', cval=0.0)
+        y_sum = ndimage.convolve(y, kernel)  # , mode='constant', cval=0.0)
+        z_sum = ndimage.convolve(z, kernel)  # , mode='constant', cval=0.0)
+        total = np.sqrt(x_sum * x_sum + y_sum * y_sum + z_sum * z_sum)
+        ruggedness = 1.0 - (total / kernel_size)
+        pixelBlocks["output_pixels"] = ruggedness.astype(props["pixelType"], copy=False)
         return pixelBlocks
-
 
     def updateKeyMetadata(self, names, bandIndex, **keyMetadata):
         if bandIndex == -1:
-            keyMetadata['datatype'] = 'Processed'               # outgoing raster is now 'Processed'
+            keyMetadata["datatype"] = "Processed"  # outgoing raster is now 'Processed'
         elif bandIndex == 0:
-            keyMetadata['wavelengthmin'] = None                 # reset inapplicable band-specific key metadata
-            keyMetadata['wavelengthmax'] = None
+            keyMetadata[
+                "wavelengthmin"
+            ] = None  # reset inapplicable band-specific key metadata
+            keyMetadata["wavelengthmax"] = None
         return keyMetadata
